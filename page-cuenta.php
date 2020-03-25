4fba1b4f4d1164e2bd05eba386f1b48750569c66
solucioneswebig <alexanderr677@gmail.com>
2020-03-25 13:20:40 -0400

    Update 25/03/2020 13:17

--- page-cuenta.php
+++ page-cuenta.php
@@ -24,7 +24,16 @@ include "includes/head.php";
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <?php the_content(); ?>
+          <?php 
+          if ( have_posts() ) : 
+            while ( have_posts() ) : the_post();
+          
+          the_content(); 
+          
+            endwhile;
+            endif;
+         
+          ?>
         </div>
       </div>
     </div>
