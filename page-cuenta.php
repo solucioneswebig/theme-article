d55731d605a793e98414e61e552127c908622eae
guty1997 <guttyy1997@gmail.com>
2020-03-25 23:45:30 -0400

    ajustes de theme-article 11:48pm 25/03/2020

--- page-cuenta.php
+++ page-cuenta.php
@@ -27,7 +27,7 @@ include "includes/head.php";
 
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-md-11">
+        <div class="col-md-8">
           <?php 
           if ( have_posts() ) : 
             while ( have_posts() ) : the_post();
