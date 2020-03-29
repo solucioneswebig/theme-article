7dcc129cd27ea05dd44c4ddaf2a904d6296327c8
solucioneswebig <alexanderr677@gmail.com>
2020-03-29 15:59:34 -0400

    Login and register

--- functions.php
+++ functions.php
@@ -239,3 +239,43 @@ function bbloomer_separate_login_form() {
    woocommerce_login_form();
    return ob_get_clean();
 }
+
+
+
+add_shortcode( 'wc_forgot_password_dashboard', 'wc_forgot_password' );
+    
+function wc_forgot_password() {
+if ( is_admin() ) return;
+if ( is_user_logged_in() ) return;
+ob_start();
+
+// NOTE: THE FOLLOWING <FORM></FORM> IS COPIED FROM woocommerce\templates\myaccount\form-login.php
+// IF WOOCOMMERCE RELEASES AN UPDATE TO THAT TEMPLATE, YOU MUST CHANGE THIS ACCORDINGLY
+
+?>
+<form method="post" class="woocommerce-ResetPassword lost_reset_password">
+
+	<p><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
+
+	<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
+		<label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?></label>
+		<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" />
+	</p>
+
+	<div class="clear"></div>
+
+	<?php do_action( 'woocommerce_lostpassword_form' ); ?>
+
+	<p class="woocommerce-form-row form-row">
+		<input type="hidden" name="wc_reset_password" value="true" />
+		<button type="submit" class="woocommerce-Button button" value="<?php esc_attr_e( 'Reset password', 'woocommerce' ); ?>"><?php esc_html_e( 'Reset password', 'woocommerce' ); ?></button>
+	</p>
+
+	<?php wp_nonce_field( 'lost_password', 'woocommerce-lost-password-nonce' ); ?>
+
+</form>
+
+<?php
+     
+return ob_get_clean();
+}
