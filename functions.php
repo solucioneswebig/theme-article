43f4e426ad568ca611010b12879e9e2557babea3
solucioneswebig <alexanderr677@gmail.com>
2020-03-29 16:20:49 -0400

    Login and register

--- functions.php
+++ functions.php
@@ -236,7 +236,42 @@ function bbloomer_separate_login_form() {
    if ( is_admin() ) return;
    if ( is_user_logged_in() ) return; 
    ob_start();
-   woocommerce_login_form();
+
+    ?>
+
+<form class="woocommerce-form woocommerce-form-login login" method="post">
+
+<?php do_action( 'woocommerce_login_form_start' ); ?>
+
+<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+  <label for="username"><?php esc_html_e( 'Username or email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+  <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
+</p>
+<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+  <label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+  <input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" autocomplete="current-password" />
+</p>
+
+<?php do_action( 'woocommerce_login_form' ); ?>
+
+<p class="form-row">
+  <label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
+    <input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
+  </label>
+  <?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+  <button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Log in', 'woocommerce' ); ?></button>
+</p>
+<p class="woocommerce-LostPassword lost_password">
+  <a href="<?php echo get_site_url(); ?>/lost-password/"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
+  <a href="<?php echo get_site_url(); ?>/register/"><?php esc_html_e( 'Register', 'woocommerce' ); ?></a>
+</p>
+
+<?php do_action( 'woocommerce_login_form_end' ); ?>
+
+</form>
+
+    <?php
+
    return ob_get_clean();
 }
 
